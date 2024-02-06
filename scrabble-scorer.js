4a804ce0501b1f85625a4cf9bae93c66e46614e9
unknown <rahulpksu@gmail.com>
2024-02-05 21:53:28 -0600

    last part to work on is scrabblescorer function

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -25,7 +25,9 @@ function oldScrabbleScorer(word) {
 		 }
  
 	  }
+
 	}
+
 	return letterPoints;
  }
 
@@ -34,25 +36,95 @@ function oldScrabbleScorer(word) {
 
 function initialPrompt() {
    console.log("Let's play some scrabble! Enter a word:");
+
+   //grabs the word user enters
+   let word = input.question('Enter a word to score: '); 
+
+   return word 
 };
 
-let simpleScorer;
+let simpleScorer = function(word) {
+   return word.length;  
+};
 
-let vowelBonusScorer;
+let vowelBonusScorer = function(word) {
+   word = word.toLowerCase();
+   let points = 0;
 
-let scrabbleScorer;
+   //iterates through word length
+   for(i = 0; i < word.length; i++) {
 
-const scoringAlgorithms = [];
+      //checks to see if the letter in the word is a vowel starting with first letter
+      if(word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u') {
+         points = points + 3;
+      } else {
+         points = points + 1;
+      }
 
-function scorerPrompt() {}
+   }
+   return points;
+};
 
-function transform() {};
+let scrabbleScorer = function(){
+   //to work on - not entirely sure what to do here
+   //name implies it should return a score
+   //should this be using transform function?
+   
+};
 
-let newPointStructure;
+const scoringAlgorithms = [
+   {
+      name: "Simple Scorer",
+      description: "Each letter is worth 1 point.",
+      scorerFunction: simpleScorer
+   },
+   {
+      name: "Bonus Vowels",
+      description: "Vowels are 3 pts, consonants are 1 pt.",
+      scorerFunction: vowelBonusScorer
+   },
+   {
+      name: "Scrabble",
+      description: "The traditional scoring algorithm.",
+      scorerFunction: scrabbleScorer
+   }
+];
+
+function scorerPrompt() {
+   let response = input.question(`Which scoring system would you like to use? \n
+   Press 0 for ${scoringAlgorithms[0].name}: ${scoringAlgorithms[0].description} \n
+   Press 1 for ${scoringAlgorithms[1].name}: ${scoringAlgorithms[1].description} \n
+   Press 2 for ${scoringAlgorithms[2].name}: ${scoringAlgorithms[2].description} \n`);
+   
+   //returns reference to scoring function to use
+   return scoringAlgorithms[response].scorerFunction
+}
+
+function transform(pointStructure) {
+   let transformedObj = {};
+
+   for (let point in pointStructure) { //iterate through every point in the object
+
+      //iterate through the array in each key
+      for(i = 0; i < pointStructure[point].length; i++){ 
+         const letter = pointStructure[point][i].toLowerCase() //make this easiser to read and makes the letter lower case
+         transformedObj[letter] = parseInt(point); //parseInt() uses a string argument and returns an integer we need to reference values in the obj easily
+      }
+   }
+
+   return transformedObj
+};
+
+let newPointStructure = transform(oldPointStructure);
+
 
 function runProgram() {
-   initialPrompt();
-   
+   //no logic in here
+   const word = initialPrompt();
+   const algorithmFn = scorerPrompt();
+   const score = algorithmFn(word);
+
+   console.log(`You scored ${score} points with your word: ${word}`)
 }
 
 // Don't write any code below this line //
